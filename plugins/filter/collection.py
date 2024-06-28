c71961eba6197fc4078aab75c2355ac9cebee041
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2024-06-28 10:14:15 +0100

    Empty commit message

--- plugins/filter/collection.py
+++ plugins/filter/collection.py
@@ -1,6 +1,155 @@
 import hashlib
 from ansible.errors import AnsibleFilterError
 
+def hash_md5(content):
+    return hashlib.md5(str(content).encode()).hexdigest()
+
+def data_get(data, key, default=None):
+    if key is None:
+        return data
+
+    if isinstance(key, str):
+        key = key.split('.')
+
+    for i, segment in enumerate(key):
+        key.pop(i)
+
+        if segment is None:
+            return data
+
+        if segment == '*':
+            if not isinstance(data, (list, dict)):
+                return default
+
+            result = []
+
+            for item in data:
+                result.append(data_get(item, key))
+
+            return [item for sublist in result for item in sublist] if '*' in key else result
+
+        if segment == '\\*':
+            segment = '*'
+        elif segment == '\\{first}':
+            segment = '{first}'
+        elif segment == '{first}':
+            if isinstance(data, dict):
+                segment = next(iter(data))
+            elif isinstance(data, list):
+                segment = 0
+            else:
+                return default
+        elif segment == '\\{last}':
+            segment = '{last}'
+        elif segment == '{last}':
+            if isinstance(data, dict):
+                segment = next(reversed(data))
+            elif isinstance(data, list):
+                segment = len(data) - 1
+            else:
+                return default
+
+        if isinstance(data, dict) and segment in data:
+            data = data[segment]
+        elif isinstance(data, list) and isinstance(segment, int) and 0 <= segment < len(data):
+            data = data[segment]
+        else:
+            return default
+
+    return data
+
+def data_set(data, key, value, overwrite=True):
+    if isinstance(key, str):
+        segments = key.split('.')
+    else:
+        segments = key
+
+    segment = segments.pop(0)
+
+    if segment == '*':
+        if not isinstance(data, (list, dict)):
+            data = []
+
+        if segments:
+            if isinstance(data, dict):
+                keys = list(data.keys())
+                for k in keys:
+                    data[k] = data_set(data[k], segments.copy(), value, overwrite)
+            elif isinstance(data, list):
+                for i in range(len(data)):
+                    data[i] = data_set(data[i], segments.copy(), value, overwrite)
+        elif overwrite:
+            if isinstance(data, dict):
+                for k in data:
+                    data[k] = value
+            elif isinstance(data, list):
+                for i in range(len(data)):
+                    data[i] = value
+
+    elif isinstance(data, dict):
+        if segments:
+            if segment not in data:
+                data[segment] = {}
+            data[segment] = data_set(data[segment], segments.copy(), value, overwrite)
+        elif overwrite or segment not in data:
+            data[segment] = value
+
+    elif isinstance(data, list):
+        if segment.isdigit():
+            index = int(segment)
+            if segments:
+                if index >= len(data):
+                    data.extend([{}] * (index - len(data) + 1))
+                data[index] = data_set(data[index], segments.copy(), value, overwrite)
+            elif overwrite or index >= len(data) or data[index] is None:
+                if index >= len(data):
+                    data.extend([None] * (index - len(data) + 1))
+                data[index] = value
+
+    else:
+        data = {}
+        if segments:
+            data[segment] = data_set({}, segments.copy(), value, overwrite)
+        elif overwrite:
+            data[segment] = value
+
+    return data
+
+def data_forget(data, key):
+    if isinstance(key, str):
+        segments = key.split('.')
+    else:
+        segments = key
+
+    segment = segments.pop(0)
+
+    if segment == '*' and isinstance(data, (list, dict)):
+        if segments:
+            if isinstance(data, dict):
+                for k in list(data.keys()):
+                    data_forget(data[k], segments.copy())
+            elif isinstance(data, list):
+                for i in range(len(data)):
+                    data_forget(data[i], segments.copy())
+    elif isinstance(data, dict):
+        if segments and segment in data:
+            data_forget(data[segment], segments.copy())
+        elif segment in data:
+            del data[segment]
+    elif isinstance(data, list) and segment.isdigit():
+        index = int(segment)
+        if segments and 0 <= index < len(data):
+            data_forget(data[index], segments.copy())
+        elif 0 <= index < len(data):
+            del data[index]
+    elif hasattr(data, segment):
+        if segments:
+            data_forget(getattr(data, segment), segments.copy())
+        else:
+            delattr(data, segment)
+
+    return data
+
 def keys_only(data, target):
     """Return only the keys of the given dictionary that are in the provided list."""
     if not isinstance(data, dict):
@@ -98,25 +247,18 @@ def flatten_query(data, keyAttribute, valAttribute=None, assignChar='=', joinCha
                 result.append(f"{item[keyAttribute]}")
     return joinChar.join(result)
 
-def unique_recursive(data, attributes, recurse=None):
-    excluded = []
+def unique_by_attribute(data, attribute):
+    seen = set()
+    result = []
+    for item in data:
+        if attribute not in item:
+            result.append(item)
+        elif item.get(attribute) not in seen:
+            result.append(item)
+            seen.add(item.get(attribute))
+    return result
 
-    def simple_hash(content):
-        return hashlib.md5(str(content).encode()).hexdigest()
-        
-    def unique_by_attr(items, attr):
-        seen = set()
-        result = []
-        for item in items:
-            item_hash_content = str(attr) + ':|:' + str(item.get(attr))
-            item_hash = simple_hash(item_hash_content)
-            if item_hash not in seen:
-                result.append(item)
-                seen.add(item_hash)
-            else:
-                excluded.append([item, attr, item_hash_content, item_hash])
-        return result
-
+def _unique_recursive(data, attribute, recurse=None):
     def get_nested_value(item, nested_key):
         keys = nested_key.split('.')
         value = item
@@ -136,51 +278,60 @@ def unique_recursive(data, attributes, recurse=None):
             d = d[key]
         d[keys[-1]] = value
 
+    for item in data:
+        if isinstance(item, dict) and recurse:
+            nested_value = get_nested_value(item, recurse)
+            if isinstance(nested_value, list):
+                unique_nested_value = unique_recursive(nested_value, attribute, recurse)
+                set_nested_value(item, recurse, unique_nested_value)
+    return unique_by_attribute(data, attribute)
+
+def unique_recursive(data, attributes, recurse=None):
+    if not (isinstance(data, list) or all(isinstance(entry, dict) for entry in data)):
+        raise AnsibleFilterError("unique_recursive expects a list of dictionaries")
+    
+    if not isinstance(attributes, (str, list)):
+        raise AnsibleFilterError("attributes should be a string or list")
+    
+    if isinstance(attributes, str):
+        attributes = [attributes]
+    
+    for attr in attributes:
+        data = _unique_recursive(data, attr, recurse)
+
+    return data
+
+def selectattr_defined(data, attributes, logic='and'):
+    if not isinstance(data, list):
+        raise AnsibleFilterError("selectattr_defined expects a list of dictionaries")
+
     if isinstance(attributes, str):
         attributes = [attributes]
 
-    explored = []
-    if isinstance(data, list):
-        for attribute in attributes:
-            explored.append(attribute)
-            for item in data:
-                if isinstance(item, dict) and recurse:
-                    nested_value = get_nested_value(item, recurse)
-                    if isinstance(nested_value, list):
-                        unique_nested_value = unique_recursive(nested_value, attribute, recurse)
-                        set_nested_value(item, recurse, unique_nested_value)
-        return [unique_by_attr(data, attribute), explored]
-    else:
-        raise AnsibleFilterError("unique_recursive expects a list of dictionaries")
+    if not all(isinstance(attr, str) for attr in attributes):
+        raise AnsibleFilterError("attributes should be a string or list of strings")
 
-def selectattr_defined(data, attributes, logic='and'):
-        if not isinstance(data, list):
-            raise AnsibleFilterError("selectattr_defined expects a list of dictionaries")
+    if logic not in ['and', 'or']:
+        raise AnsibleFilterError("logic should be either 'and' or 'or'")
 
-        if isinstance(attributes, str):
-            attributes = [attributes]
+    result = []
+    for item in data:
+        if isinstance(item, dict):
+            if logic == 'and':
+                if all(attr in item for attr in attributes):
+                    result.append(item)
+            elif logic == 'or':
+                if any(attr in item for attr in attributes):
+                    result.append(item)
 
-        if not all(isinstance(attr, str) for attr in attributes):
-            raise AnsibleFilterError("attributes should be a string or list of strings")
-
-        if logic not in ['and', 'or']:
-            raise AnsibleFilterError("logic should be either 'and' or 'or'")
-
-        result = []
-        for item in data:
-            if isinstance(item, dict):
-                if logic == 'and':
-                    if all(attr in item for attr in attributes):
-                        result.append(item)
-                elif logic == 'or':
-                    if any(attr in item for attr in attributes):
-                        result.append(item)
-
-        return result
-
+    return result
+    
 class FilterModule(object):
     def filters(self):
         return {
+            'data_get': data_get,
+            'data_set': data_set,
+            'data_forget': data_forget,
             'keys_only': keys_only,
             'keys_except': keys_except,
             'only_with': only_with,
