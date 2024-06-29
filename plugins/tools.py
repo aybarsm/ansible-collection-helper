a1dd7c55dd6bae179dd55bacdf5414e4d0352c38
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2024-06-29 19:13:35 +0100

    Arrangements with requirements.txt

--- plugins/tools.py
+++ plugins/tools.py
@@ -1,28 +1,6 @@
 from ansible.errors import AnsibleFilterError
-import hashlib
-import uuid
-import datetime
 
-class Tools:
-
-    @staticmethod
-    def hash_md5(content):
-        return hashlib.md5(str(content).encode()).hexdigest()
-
-    @staticmethod
-    def uuid_4():
-        return str(uuid.uuid4())
-    
-    @staticmethod
-    def timestamp_iso(format="%Y-%m-%dT%H:%M:%SZ", UTC=True):
-        if UTC:
-            return datetime.datetime.now(datetime.UTC).strftime(format)
-        return datetime.datetime.now().strftime(format)
-    
-    @staticmethod
-    def generate_unique_salt(seperator="|", ts_format="%Y-%m-%dT%H:%M:%S.%fZ"):
-        return str(Tools.timestamp_iso(format=ts_format) + seperator + Tools.uuid_4())
-    
+class Tools:    
     @staticmethod
     def merge_dicts(baseDict, *args):
         newDict = baseDict.copy()
