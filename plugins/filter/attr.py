65b2cb381a861bb059546003a6afa7764e15b6a9
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2024-06-30 22:17:13 +0100

    Merge splitattr and joinattr into Attr class

--- plugins/filter/attr.py
+++ plugins/filter/attr.py
@@ -21,39 +21,60 @@ from jinja2.filters import pass_environment
 #   - ['state', 'equalto', 'present']
 # logic: and (optional | default is 'and')
 
+# Example config for splitattr:
+# List of dictionaries
+# - srcAttr: name (required)
+#   dstAttr: version (required)
+#   search: = (required)
+#   overwrite: true (optional | default is True)
+#   dstSide: right (optional | default is 'right')
+#   renameSrcAttr: package (optional)
+#   dstDefault: None (optional)
+#   when: (optional)
+#     - ['name', 'defined']
+#     - ['version', 'undefined']
+#   logic: and (optional | default is 'and')
+
+# Example config for joinattr:
+# List of dictionaries
+# - leftAttr: name (required)
+#   rightAttr: version (required)
+#   join: = (required)
+#   overwrite: true (optional | default is True)
+#   dstAttr: name (optional | default is leftAttr)
+#   deleteSrcAttrs: False (optional | default is False)
+#   when: (optional)
+#     - ['name', 'defined']
+#     - ['version', 'undefined']
+#   logic: and (optional | default is 'and')
+
 class Attr:
-    def __init__(self, action, data, configs, jinja_env, cnf_defaults={}):
+    def __init__(self, jinja_env, action, data, configs, required, cnf_defaults={}):
+        self.jinja = JinjaEnv(jinja_env)
         self.action = action
         self.data = data
         self.configs = configs
+        self.required = required
         self.cnf_defaults = cnf_defaults
-        self.jinja = JinjaEnv(jinja_env)
         
-        self.validate_common()
-        if action == 'set':
-            self.validate_set()
-            self.result = data.copy()
-        elif action in ['select', 'reject']:
-            self.validate_select_or_reject()
+        self.validate_and_prep()
+
+        if action in ['select', 'reject']:
             self.result = []
+        else:
+            self.result = data.copy()
 
-        self.prep_configs()
-
-    def prep_configs(self):
-        if self.action in ['select', 'reject']:
-            self.configs = [self.configs]
-
-        self.configs = list(map(lambda cnf: Dict.merge(self.cnf_defaults, cnf), self.configs))
-    
-    def validate_common(self):
+    def validate_and_prep(self):
         if not (isinstance(self.data, list) or all(isinstance(item, dict) for item in self.data)):
             raise AnsibleFilterError("data must be a list of dictionaries")
-
-    def validate_set(self):
-        if not isinstance(self.configs, list) or not all(isinstance(item, dict) for item in self.configs):
-            raise AnsibleFilterError("configuration must be a list of dictionaries")
         
         for cnf in self.configs:
+            if not isinstance(cnf, dict):
+                raise AnsibleFilterError("configuration elements must be dictionaries")
+            
+            if not all(key in cnf for key in self.required):
+                raise AnsibleFilterError(f"configuration elements must have all required keys: {', '.join(self.required)}")
+            
             if 'attribute' not in cnf or 'value' not in cnf:
                 raise AnsibleFilterError("configuration elements must have 'attribute' and 'value' keys")
 
@@ -108,28 +129,48 @@ class Attr:
         return self.result
     
 def select_or_reject_attr(environment,data, configs, reject=False):
+    required = ['when']
     cnf_defaults = {'logic': 'and'}
     
     action = 'select' if not reject else 'reject'
-    attr = Attr(action, data, configs, environment, cnf_defaults)
+    attr = Attr(environment, 'set', data, configs, required, cnf_defaults)
 
     return attr.run()
 
 @pass_environment      
 def setattr(environment, data, configs):
+    required = ['attribute', 'value']
     cnf_defaults = {'logic': 'and', 'when':[], 'overwrite': False, 'deleteWhenNone': False}
     
-    attr = Attr('set', data, configs, environment, cnf_defaults)
+    attr = Attr(environment, 'set', data, configs, required, cnf_defaults)
 
     return attr.run()
 
 @pass_environment
-def selectattr(environment, data, config):
-    return select_or_reject_attr(environment, data, config)
+def selectattr(environment, data, configs):
+    return select_or_reject_attr(environment, data, configs)
 
 @pass_environment
-def rejectattr(environment, data, config):
-    return select_or_reject_attr(environment, data, config, True)
+def rejectattr(environment, data, configs):
+    return select_or_reject_attr(environment, data, configs, True)
+
+@pass_environment      
+def splitattr(environment, data, configs):
+    required = ['srcAttr', 'dstAttr', 'search']
+    cnf_defaults = {'overwrite': True, 'dstSide': 'right', 'when': [], 'logic': 'and'}
+    
+    attr = Attr(environment, 'set', data, configs, required, cnf_defaults)
+
+    return attr.run()
+
+@pass_environment      
+def joinattr(environment, data, configs):
+    required = ['leftAttr', 'rightAttr', 'join']
+    cnf_defaults = {'overwrite': True, 'deleteSrcAttrs': False, 'when': [], 'logic': 'and'}
+    
+    attr = Attr(environment, 'set', data, configs, required, cnf_defaults)
+
+    return attr.run()
 
 class FilterModule(object):
     def filters(self):
